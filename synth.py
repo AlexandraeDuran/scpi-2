efd59a223bd9c8b14ebadce9b63bb7bdcc18e3a7
TPH <thomas.harty@physics.ox.ac.uk>
2018-04-20 17:14:35 +0100

    add power and rf output functions.

--- synth.py
+++ synth.py
@@ -18,10 +18,24 @@ class Synth:
         self.stream.write("FREQ?\n".encode())
         return self.stream.readline().decode()
 
+    def set_power(self, power):
+        self.stream.write("POW {} DBM\n".format(power).encode())
+
+    def get_power(self):
+        self.stream.write("POW?\n".encode())
+        return float(self.stream.readline().decode())
+
+    def set_rf_on(self, enabled):
+        self.stream.write("OUTP {:d}\n".format(int(enabled)).encode())
+
+    def get_rf_on(self):
+        self.stream.write("OUTP?\n".encode())
+        return bool(self.stream.readline().decode())
+
 if __name__ == "__main__":
     import streams
     gpib = streams.EthGPIB("10.255.6.11")
     print("Connected to {}".format(gpib.get_version()))
     synth = Synth(gpib.get_stream(25))
-    synth.set_freq(1000e3)
-    print(synth.get_freq())
+    synth.set_rf_on(False)
+    print(synth.get_rf_on())
